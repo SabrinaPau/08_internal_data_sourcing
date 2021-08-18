802681213ff3e62914ad36ee7a7cb2c289248646
Philipp Wendt <philipp.wendt@neuefische.de>
2021-08-18 11:12:18 +0200

    sql.py

--- notebooks/sql.py
+++ notebooks/sql.py
@@ -1,18 +1,9 @@
 # Fill in credentials
-host='____________'
-port='____________'
-database='________'
-user='____________'
-password='________'
-
-# psycopg2 connection for querying data from a database
-import psycopg2
-conn = psycopg2.connect(host=host,
-                        port=port,
-                        database=database,
-                        user=user,
-                        password=password)
-
+host='________'
+port='________'
+database='____'
+user='________'
+password='____'
 
 # Import the Python packages for get_data() function
 
