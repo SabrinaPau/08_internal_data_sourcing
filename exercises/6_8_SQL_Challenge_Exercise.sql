1e0fe3f357bed96401d1ea58438fd089548a4b87
konni-b <braun.konstanze@gmail.com>
2022-05-02 13:00:15 +0200

    edit of 6_8 Challenge after feedback

--- exercises/6_8_SQL_Challenge_Exercise.sql
+++ exercises/6_8_SQL_Challenge_Exercise.sql
@@ -1,6 +1,7 @@
 /* 
 * Try to get as far as you can in this challenge, but don't worry if you can't achieve all objectives.
 */
+
 /*  
  * 1 As a Data Analyst, it is crucial to be able to make sure that you can rely on the quality of your data.
  *   In the actual_elapsed_time column you have data with the calculated time from departure to arrival. 
@@ -11,18 +12,21 @@
 /*  
  * 1.1 But first, let's take a closer look at the data and get an overall view.
  * 	   Since computational power comes at some costs and is shared by many people inside a company,
- *     retrieve only that much data you really need. So, let's take a look at the first 100 rows of the flights table.
+ *     retrieve only as much data as you really need. So, let's take a look at the first 100 rows of the flights table.
  * 	   Please provide the query below.
  */
+
 
 
 /*  
  * 1.2 And now, let's take a closer look at some particular relevant columns and make sure we fully understand these values.
  * 	   What do the values in arr_time, dep_time and actual_elapsed_time really mean?
- * 	   Retrieve all unique values from these columns(in three queries) and order them in descending order.
+ * 	   Retrieve all unique values from these columns(in three separate queries) and order them in descending order.
  *     Remember, retrieve as much data as you need - not more and not less.
- * 	   Please provide the query below.
+ * 	   Please provide the queries below.
  */
+
+
 
 /*  
  * 1.3 What do the values in these three columns mean?
@@ -30,25 +34,28 @@
  */
 
 
+
 /*  
- * 2   Finally. Let's start with the actual task. In the next steps, you are going to calculate the flight time 
+ * 2   Finally. Let's start with the actual task. In the next steps, you are going to calculate the travel time 
  *     and match it with the actual_elapsed_time column values.
  * 	   ==> The main objective is to get as close as possible to a match rate of 100%.
  */
 
  /*
- * 2.1 Query the following columns from the flights table: flight_date, origin, dest, dep_time, arr_time and air_time.
+ * 2.1 Query the following columns from the flights table: flight_date, origin, dest, dep_time, arr_time and actual_elapsed_time.
  *     Convert dep_time, arr_time into TIME variables: dep_time_f and arr_time_f 
- *     Convert air_time into an INTERVAL variable: air_time_f
+ *     Convert actual_elapsed_time into an INTERVAL variable: actual_elapsed_time_f
  *     Calculate the difference of dep_time_f and arr_time_f in a new column called travel_time_f.
  *     Please provide the query below.
  */
 
 
-/* 2.2 Compare the travel time with the air_time column.
+
+/* 2.2 Compare the travel_time_f column with the actual_elapsed_time_f column.
  * 	   How many values are matching? Provide the number in percentage.
  *     Please provide the query and answer below.
  */
+
 
 
 /* 2.3 Try to explain the results of 2.2.
@@ -56,30 +63,35 @@
  */
 
 
+
 /* 2.4 Join the airports table and add the time zone columns to your existing table.
  * 	   Before you add them, transform them to INTERVAL and change their names to origin_tz and dest_tz.
  *     Please provide the query below.
  */
+
 
 	  
 /* 2.5 Next, convert the departure and arrival time to UTC and store them in dep_time_f_utc and arr_time_f_utc.
  * 	   Calculate the difference of the new columns and store it in a new column travel_time_f_utc.
  *     Please provide the query below.
  */
-	  
+
+
 
 /* 2.6 What's the percentage of matching records now?
  *     Explain the result.
  *     Please provide the query and answer below.
  */
- 
+
+	  
 
 /* Extra Credit: 2.7 Add two columns to your table
- * 				     dep_timestamp_utc: a timestamp that shows the date and time of the departure in UTC time zone
- *     			     arr_timestamp_utc: a timestamp that shows the date and time of the arrival in UTC time zone
- *     			     How many flights arrived after midnight UTC?
- *     			     Please provide the query and answer below.
+ * 				   	 dep_timestamp_utc: a timestamp that shows the date and time of the departure in UTC time zone
+ *     			   	 arr_timestamp_utc: a timestamp that shows the date and time of the arrival in UTC time zone
+ *     			   	 How many flights arrived after midnight UTC?
+ *     			   	 Please provide the query and answer below.
  */
+
 
 
 /* Extra Credit: 2.8 Until now, we achieved a match rate of nearly 84%
@@ -87,4 +99,3 @@
  *  				 2. Create a query and confirm your ideas.  
  */
 
-	  
