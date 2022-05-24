59a3ca1096936f6333ce97f092d757e83217c599
adriz-nf <andrewemmett.zz@gmail.com>
2022-05-24 12:54:15 +0200

    rebuilding python and sql nb two thirds done revising

--- notebooks/sql_functions.py
+++ notebooks/sql_functions.py
@@ -1,8 +1,17 @@
 # We import a method from the  modules to address environment variables and 
-# we use that method to save the variables from .env to a dictionary we call sql_config
+# we use that method in a function that will return the variables we need from .env 
+# to a dictionary we call sql_config
+
 from dotenv import dotenv_values
 
-sql_config = dotenv_values(".env")
+def get_sql_config():
+    '''
+        Function loads credentials from .env file and
+        returns a dictionary containing the data needed for sqlalchemy.create_engine()
+    '''
+    dotenv_dict = dotenv_values(".env")
+    sql_config = {key:dotenv_dict[key] for key in ('host', 'host','database','user','password') if key in dotenv_dict}
+    return sql_config
 
 # Import the Python packages for get_data() function - worry about this only when we say so in the notebook
 
