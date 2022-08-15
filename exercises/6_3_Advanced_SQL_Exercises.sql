66c02ee864e9995519049b48d3c6e93c8be6c59a
philippwendt <80899780+philippwendt@users.noreply.github.com>
2022-08-15 11:59:10 +0200

    Update 6_3_Advanced_SQL_Exercises.sql
    Make clear windows functions topic is optional

--- exercises/6_3_Advanced_SQL_Exercises.sql
+++ exercises/6_3_Advanced_SQL_Exercises.sql
@@ -36,8 +36,8 @@
 
 
 /* Q3.1 Filter the data to January 1, 2021 and count all rows for that day so that your result set has two columns: flight_date, total_flights.  
- * 		Repeat this step, but this time only include data from January 2, 2021.
- * 		Combine the two result sets using UNION.
+ * 		   Repeat this step, but this time only include data from January 2, 2021.
+ * 		   Combine the two result sets using UNION.
  */
 
 
@@ -48,19 +48,19 @@
 
 
 /* Q3.3 Take your query from Q3.1 and replace the UNION operator with the INTERSECT operator.
- * 		Explain your results.
+ * 		   Explain your results.
  */
 
 
 
 /* Q3.4 Take your query from Q3.1 and replace the UNION operator with the EXCEPT operator.
- * 		Explain your results.
+ * 		   Explain your results.
  */
 
 
 
 /* Q4. What's the highest number of flights that have departed in a day from an airport above altitude 7800?
- *	   You are NOT allowed to use INNER, LEFT, RIGHT, FULL or CROSS joins!
+ *	    You are NOT allowed to use INNER, LEFT, RIGHT, FULL or CROSS joins!
  * 	   Please provide the query and answer below.
  */
 
@@ -68,7 +68,7 @@
 
 /* Q5.1 How many flights have departed and arrived in the United States?
  *      You are NOT allowed to use INNER, LEFT, RIGHT, FULL or CROSS joins!
- * 		Please provide the query and answer below.
+ * 		   Please provide the query and answer below.
  */
 
 
@@ -102,51 +102,51 @@
 
 
 
-//* BONUS: Advanced Aggregations using Window Functions
+//* OPTIONAL: Advanced Aggregations using Window Functions
  * Q8.1 The airline American Airlines (AA) wants you to take a look at one of their planes: N825AW
- * 		They want you to provide a list with 4 columns: flight_date, tail_number, arr_delay, acc_flight_delay.
- * 		The 'acc_flight_delay' should calculate the running sum of the arr_delay from beginning to the end of the month
- * 		The list should be sorted by the flight date starting with the earliest date
- * 		Please provide the query below and answer the following question:
- * 		How much delay time has the plane accumulated on its last day?
+ * 		   They want you to provide a list with 4 columns: flight_date, tail_number, arr_delay, acc_flight_delay.
+ * 		   The 'acc_flight_delay' should calculate the running sum of the arr_delay from beginning to the end of the month
+ * 		   The list should be sorted by the flight date starting with the earliest date
+ * 		   Please provide the query below and answer the following question:
+ * 		   How much delay time has the plane accumulated on its last day?
  */
 
 
 
 /* Q8.2 After American Airlines looked at the output you gave them, they realised that the arrival delay
  *      is heavily influenced by the departure delay. If a plane already departed late then it has a high
- * 		chance of arriving late. That's why they want you to add the column dep_delay to the list and calculate
- * 		the net_flight_delay which is the difference of arr_delay and dep_delay.
- * 		Change the acc_flight_delay to acc_net_fligt_delay and adjust the calculation accordingly.
- * 		How big is the difference between the previously calculated arr_delay and the net_flight_delay?
- * 		Please provide the query and answer below.
+ * 		   chance of arriving late. That's why they want you to add the column dep_delay to the list and calculate
+ * 		   the net_flight_delay which is the difference of arr_delay and dep_delay.
+ * 		   Change the acc_flight_delay to acc_net_fligt_delay and adjust the calculation accordingly.
+ * 		   How big is the difference between the previously calculated arr_delay and the net_flight_delay?
+ * 		   Please provide the query and answer below.
  */
 
 
 
 /* Q8.3 American Airlines has one more request: Please summarise the previous output and provide
- * 		them with a table that has flight_date, tail_number and the sum of net_flight_delay grouped
- * 		by flight_date and tail_number.
+ * 		   them with a table that has flight_date, tail_number and the sum of net_flight_delay grouped
+ * 		   by flight_date and tail_number.
  */
 
 
 
 /* Q8.4 They love it! Good job! Since your work has been very helpful to them they want to expand the output to
- * 		more planes that they own. Please add the following planes in your output: N825AW, N756AM, N9018E.
- * 		Additionally they would like you to add an additional column to the output called net_flight_delay_cat.
- *		This column should one of three categories depending on the value of net_flight_delay.
- *		If net_flight_delay > 0 then '1-Slower'
- *		   net_flight_delay = 0 then '2-As_expected'
- *		   else '3-Faster'.
- *		Please provide the query below.
+ * 		   more planes that they own. Please add the following planes in your output: N825AW, N756AM, N9018E.
+ * 		   Additionally they would like you to add an additional column to the output called net_flight_delay_cat.
+ *		    This column should one of three categories depending on the value of net_flight_delay.
+ *		    If net_flight_delay > 0 then '1-Slower'
+ *		    net_flight_delay = 0 then '2-As_expected'
+ *		    else '3-Faster'.
+ *		    Please provide the query below.
  */
 
 
 
 /* Q8.5 You've been doing an amazing job, American Airlines has one last request for you:
- * 		Please summarise the previous output so that the final output groups by the tail_number
- * 		and the dep_delay_cat and aggregates the number of flights in each category in a column
- * 		called total_flights and the average flight delay in the column avg_daily_flight_delay.
+ * 		   Please summarise the previous output so that the final output groups by the tail_number
+ * 		   and the dep_delay_cat and aggregates the number of flights in each category in a column
+ * 		   called total_flights and the average flight delay in the column avg_daily_flight_delay.
  */
 
 
